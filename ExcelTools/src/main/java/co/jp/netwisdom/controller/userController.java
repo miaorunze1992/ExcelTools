6d444cd1f6aa13c0af54a1a1191e072e0dac73fd
MYAO\Administrator <miao.runze@netwisdom.co.jp>
2021-11-11 19:27:55 +0900

    Excel模板简单实现,针对性替换未完成

--- ExcelTools/src/main/java/co/jp/netwisdom/controller/userController.java
+++ ExcelTools/src/main/java/co/jp/netwisdom/controller/userController.java
@@ -1,7 +1,7 @@
 package co.jp.netwisdom.controller;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Workbook;
+
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URLEncoder;
 
 @RestController
@@ -30,31 +27,34 @@ public class userController {
      */
     @GetMapping("/downloadDocument")
     public void downloadTemplate(HttpServletResponse response, String templateId) throws Exception {
-
+        // templateId未指定
         if (templateId == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
+        // 资源路径
         String resourcePath;
+        // 根据前台templateId进行区分
         switch (templateId) {
             case "001":
                 resourcePath = String.format("excelTemplate/%s.xlsx", "001");
-                downloadFile(response, resourcePath, NW_EXCEL_001);
+                downloadExcelFile(response, resourcePath, NW_EXCEL_001);
                 break;
             case "002":
                 resourcePath = String.format("excelTemplate/%s.xlsx", "002");
-                downloadFile(response, resourcePath, NW_EXCEL_002);
+                downloadExcelFile(response, resourcePath, NW_EXCEL_002);
                 break;
             case "003":
                 resourcePath = String.format("excelTemplate/%s.xlsx", "003");
-                downloadFile(response, resourcePath, NW_EXCEL_003);
+                downloadExcelFile(response, resourcePath, NW_EXCEL_003);
                 break;
             case "004":
                 resourcePath = String.format("excelTemplate/%s.xlsx", "004");
-                downloadFile(response, resourcePath, NW_WORD_004);
+                downloadExcelFile(response, resourcePath, NW_WORD_004);
                 break;
             case "005":
                 resourcePath = String.format("excelTemplate/%s.xlsx", "005");
-                downloadFile(response, resourcePath, NW_WORD_005);
+                downloadExcelFile(response, resourcePath, NW_WORD_005);
                 break;
             default:
                 break;
@@ -64,20 +64,39 @@ public class userController {
 
     }
 
-    private static void downloadFile(HttpServletResponse response, String resourcePath, String fileName) throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource(resourcePath);
-        InputStream inputStream = classPathResource.getInputStream();
-        Workbook workbook;
-        try {
-            workbook = new XSSFWorkbook(inputStream);
-        } catch (Exception ex) {
-            workbook = new HSSFWorkbook(inputStream);
+    /**
+     * @param response     响应
+     * @param resourcePath excel模板路径
+     */
+    private static void downloadExcelFile(HttpServletResponse response, String resourcePath, String excelName) throws IOException {
+
+        InputStream inputStream = new ClassPathResource(resourcePath).getInputStream();
+        Workbook workbook = new XSSFWorkbook(inputStream);
+
+        Sheet sheet;
+        for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+            // 获取Sheel
+            sheet = workbook.getSheetAt(i);
+            for (int j = 0; j < sheet.getPhysicalNumberOfRows(); j++) {
+                // 获取Row
+                Row row = sheet.getRow(j);
+                for (int k = 0; k < row.getLastCellNum(); k++) {
+                    // 获取Cell
+                    Cell cell = row.getCell(k);
+                    if (cell != null && cell.getCellTypeEnum() == CellType.STRING && cell.getStringCellValue().equals("${name}")) {
+                        // 设置 Cell
+                        // 根据前台传入数据，需要针对性替换
+                        row.getCell(k).setCellValue("法外狂徒张三");
+                    }
+                }
+            }
         }
         response.setContentType("application/vnd.ms-excel");
-        response.setHeader("content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName + ".xlsx", "utf-8"));
-        response.setHeader("Access-Control-Expose-Headers", "content-Disposition");
+        response.setHeader("content-Disposition", "attachment;excelName=" + URLEncoder.encode(excelName + ".xlsx", "utf-8"));
         OutputStream outputStream = response.getOutputStream();
+
         workbook.write(outputStream);
+        // 关闭流
         outputStream.flush();
         outputStream.close();
 
